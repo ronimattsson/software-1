64b7312c7a4f31b0b70ec62faaf09bc5d6d19771
Roni Mattsson <mattsson.roni@gmail.com>
2026-02-08 16:30:28 +0200

    Tietojärjestelmään yhteydenmuodostus siirretty funktion sisään

--- Module8/Assignment2.py
+++ Module8/Assignment2.py
@@ -1,6 +1,17 @@
 import mysql.connector
 
 def get_airports_by_country(country_code):
+    yhteys = mysql.connector.connect(
+        host="localhost",
+        port="3306",
+        user="roni",
+        password="",
+        database="flight_game",
+        autocommit=True,
+        collation='utf8mb3_general_ci'
+    )
+    cursor = yhteys.cursor()
+
     sql = f"SELECT count(*), type FROM airport WHERE iso_country = '{country_code}' GROUP BY type ORDER BY type desc"
     cursor.execute(sql)
     return cursor.fetchall()
@@ -14,15 +25,5 @@ def run_country_program():
         print("\nAirports in " + code.upper() + ": ")
         for pair in result:
             print(str(pair[0]) + " " + pair[1] + " airports")
-
-yhteys = mysql.connector.connect(
-    host="localhost",
-    port ="3306",
-    user="roni",
-    password="",
-    database="flight_game",
-    autocommit=True
-    )
-cursor = yhteys.cursor()
 
 run_country_program()
