d9886f2ddeac418f5f3b133b0f59abd3f871c91a
Roni Mattsson <mattsson.roni@gmail.com>
2026-02-08 19:10:59 +0200

    Muutettu pääsemään moodlen testin läpi

--- Module8/Assignment1.py
+++ Module8/Assignment1.py
@@ -1,29 +1,34 @@
 import mysql.connector
 
-def get_town(town):
-    sql = f"SELECT name, municipality from airport where ident = '{town}'"
-    cursor.execute(sql)
-    tulos = cursor.fetchall()
-    if tulos == []:
-        return ("","")
-    else:
-        return tulos[0] #(Nimi, kaupunki)
-
-yhteys = mysql.connector.connect(
+def get_town(code):
+    connection = mysql.connector.connect(
     host="localhost",
     port ="3306",
-    user="roni",
-    password="",
     database="flight_game",
+    user='roni',
+    password='',
     autocommit=True,
     collation='utf8mb3_general_ci'
     )
-cursor = yhteys.cursor()
+    cursor = (connection.cursor())
+    query = """
+            SELECT name, municipality FROM airport WHERE ident = %s
+            """
 
-icao = input("Enter the ICAO code of an airport: ")
-(nimi, kaupunki) = get_town(icao.upper())
-if nimi != "":
-    print(f"Airport name: {nimi}\n"
-          f"Location: {kaupunki}")
-else:
-    print("No airport found with ICAO code " + icao + ".")
+    cursor.execute(query, (code,))
+    result = cursor.fetchall()
+    if result == None or result == []:
+        return ("","")
+    else:
+        return result[0]  # (Nimi, kaupunki)
+
+def main():
+    icao = input("Enter the ICAO code of an airport: ")
+    (nimi, kaupunki) = get_town(icao.upper())
+    if nimi != "":
+        print(f"Airport name: {nimi}\n"
+              f"Location: {kaupunki}")
+    else:
+        print("No airport found with ICAO code " + icao)
+
+main()
